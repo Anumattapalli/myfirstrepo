d3a84a5fca42bf7aed211970ba097f8e531c70c3
Anusree <Anusree>
2023-09-16 00:17:42 +0530

    Add changes

--- example.py
+++ example.py
@@ -1,2 +1,20 @@
-print("hello world")
-print(2)
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/u")
+def greeting():
+    return 'Hello World'
+
+@app.route("/calculator/add/<int:a>/<int:b>")
+def add(a,b):
+    return "sum is "+str(a+b)
+
+@app.route("/calculator/subtract/<int:a>/<int:b>")
+def subtract(a,b):
+    return "diff is "+str(a-b)
+
+if __name__ == '__main__':
+    app.run()
+
