a016acc6eb8d3f2ad5496ff180c1dc48901ddad9
Anusree <Anusree>
2023-09-16 11:01:31 +0530

    Add Changes

--- example.py
+++ example.py
@@ -1,20 +1,54 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
+employee_data = {}
 
-@app.route("/u")
+# Greeting
+@app.route("/greeting",methods=['GET'])
 def greeting():
-    return 'Hello World'
+    return 'Hello World!'
 
-@app.route("/calculator/add/<int:a>/<int:b>")
-def add(a,b):
-    return "sum is "+str(a+b)
+def generate_employee_id():
+    if not employee_data:
+        return 1
+    else:
+        return max(employee_data.keys()) + 1
 
-@app.route("/calculator/subtract/<int:a>/<int:b>")
-def subtract(a,b):
-    return "diff is "+str(a-b)
+# Create Employee
+@app.route('/employee', methods=['POST'])
+def create_employee():
+    data = request.json
+
+    # Generate a unique ID for the new employee
+    new_employee_id = generate_employee_id()
+
+    # Create a new employee record
+    new_employee = {
+        'employee id': new_employee_id
+        'name': data.get('name')
+        'city': data.get('city')
+    }
+
+    # Add the new employee to the data store
+    employee_data[new_employee_id] = new_employee
+
+    return jsonify(new_employee), 200  # 200 indicates resource created
+
+# Get all Employee details
+@app.route('/employees/all', methods=['GET'])
+def get_all_employees():
+    return jsonify(list(employee_data.values()))
+
+# Get Employee details
+@app.route('/employee/<int:id>', methods=['GET'])
+def get_employee(id):
+    employee = employee_data.get(id)
+    if employee:
+        return jsonify(employee)
+    else:
+        return jsonify({'error': 'Employee not found'}), 404  # 404 indicates not found
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, host='0.0.0.0')
 
